b3ad26e91bd22946b9c570347bfabc1c4713611d
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-10-17 13:30:21 +0600

    Incidents pages start

--- src/Components/pages/Incidents/Incidents.jsx
+++ src/Components/pages/Incidents/Incidents.jsx
@@ -8,6 +8,7 @@ const Incidents = () => {
         <section className="pt-5">
             <Contianer>
                 <div className="pb-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-[25px]">
+                    {/* Incidents page data */}
                     {
                         newsItem && newsItem.length > 0 ? newsItem?.map((item, idx) => <Card item={item} key={idx}/>) : (
                             <p>No, new available now</p>
