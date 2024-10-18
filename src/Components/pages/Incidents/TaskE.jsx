3ef140727de447661daf11fa79061fee26d708ac
Shakil Ahmed <shakil.devcraft@gmail.com>
2024-10-18 21:17:23 +0600

    Try to best responsive webpages

--- src/Components/pages/Incidents/TaskE.jsx
+++ src/Components/pages/Incidents/TaskE.jsx
@@ -4,10 +4,10 @@ import NavbarSecond from "../../share/NavbarSecond";
 
 const TaskE = () => {
     return (
-        <section>
+        <section className="pb-20">
             <NavbarSecond page="DR-4699 March 2023 Severe Storms" pageImg="/public/incident (17).png" text={"Sort By: Date modified"} icon2={"bx bx-plus-medical"} text2={"New Location"} />
             <Contianer>
-                <div className="flex justify-between gap-[94px] mt-2">
+                <div className="md:flex md:gap-4 lg:gap-[94px] mt-2">
                     <div className="lg:w-[680px]">
                         <div>
                             <div className="flex items-center gap-[15px]">
@@ -42,10 +42,10 @@ const TaskE = () => {
                                     {
                                         tasksE?.map(({img, title, location, amount}, idx) => (
                                             <div key={idx} className="space-y-2">
-                                                    <img src={img} alt="" />
+                                                    <img className="w-full" src={img} alt="" />
                                                     <div>
                                                         <h4 className="text-t-primary text-sm font-bold leading-[21px]">{title}</h4>
-                                                        <p className="text-t-secondary text-xs font-normal leading-[21px]">{location}</p>
+                                                        <p className="text-t-secondary text-[11px] lg:text-xs font-normal leading-[21px]">{location}</p>
                                                         <h4 className="text-t-primary text-sm font-bold leading-[21px]">{amount}</h4>
                                                     </div>              
                                             </div>
@@ -74,8 +74,58 @@ const TaskE = () => {
                                         </div>              
                                     </div>
                                 </div>
+                                <div className="bg-b-secondary rounded-[10px] mt-2">
+                                    <div className="flex justify-start items-center gap-[17px] py-[15px] px-[10px]">
+                                        <img className="size-[70px] rounded-[10px]" src="/location.png" alt="" />
+                                        <div>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">Activity name</h4>
+                                            <p className="text-t-secondary text-xs font-normal leading-[21px]">16.12212, -122.1424</p>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">$1,456,654.00</h4>
+                                        </div>              
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
+                        
+                        {/* divide */}
+                        <div className="w-full bg-b-secondary h-[1px] my-10"></div>
+
+                        <div>
+                            <div className="flex justify-between">
+                                <h4 className="text-t-primary text-sm font-bold leading-[21px]">Activities</h4>
+                                <button className="text-t-primary text-sm font-normal underline leading-[21px]">See all</button>
+                            </div>
+                            <div className="space-y-[15px]">
+                                <div className="bg-b-secondary rounded-[10px] mt-2">
+                                    <div className="flex justify-start items-center gap-[17px] py-[15px] px-[10px]">
+                                        <img className="size-[70px] rounded-[10px]" src="/location.png" alt="" />
+                                        <div>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">Document name</h4>
+                                            <p className="text-t-secondary text-xs font-normal leading-[21px]">16.12212, -122.1424</p>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">$1,456,654.00</h4>
+                                        </div>              
+                                    </div>
+                                </div>
+                                <div className="bg-b-secondary rounded-[10px] mt-2">
+                                    <div className="flex justify-start items-center gap-[17px] py-[15px] px-[10px]">
+                                        <img className="size-[70px] rounded-[10px]" src="/location.png" alt="" />
+                                        <div>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">Document name</h4>
+                                            <p className="text-t-secondary text-xs font-normal leading-[21px]">16.12212, -122.1424</p>
+                                            <h4 className="text-t-primary text-sm font-bold leading-[21px]">$1,456,654.00</h4>
+                                        </div>              
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    {/* map image */}
+                    <div className="mt-5 md:mt-0">
+                        <p className="text-t-secondary text-sm font-normal leading-[22px] mb-2.5">Incident Map</p>
+                        <img src="/map2.png" alt="" />
+                        <p className="text-t-secondary text-sm font-normal leading-[22px] mt-2.5">Start 19.1232, -118.233     End 19.3245, -119.2323</p>
                     </div>
                 </div>
             </Contianer>
--- src/Components/share/NavbarSecond.jsx
+++ src/Components/share/NavbarSecond.jsx
@@ -13,17 +13,17 @@ const NavbarSecond = ({ page="back", pageImg, text, icon, onClick, className, te
     return (
         <section className="bg-b-secondary">
             <Contianer>
-                <div className="md:flex justify-between items-center ">
+                <div className="md:flex justify-between items-center">
                     <div>
                         <span className="text-[12px] leading-[15.3px] text-t-secondary">{`Welcome ${location?.pathname !== "/" ? `- ${page}` : "back"}`}</span>
-                        <h2 className=" flex gap-3 items-center font-bold text-t-primary md:text-xl lg:text-[26px]">{pageImg ? <img src={pageImg} alt="" /> : ""} {page}</h2>
+                        <h2 className=" flex gap-3 items-center font-bold text-t-primary md:text-lg lg:text-xl lg:text-[26px] mt-2">{pageImg ? <img src={pageImg} alt="" /> : ""} {page}</h2>
                     </div>
-                    <div className="md:flex justify-between items-center gap-3 mt-5 md:mt-0">
+                    <div className="md:flex justify-between items-center md:gap-3 mt-5 md:mt-0">
                         <div className="bg-white flex justify-between items-center gap-2 py-2.5 px-4 rounded-lg mb-5 md:mb-0">
                             <i className='bx bx-search-alt-2 text-xl'></i>
                             <input className="outline-none text-[12px] font-bold flex-1" type="text" placeholder="Search incident" />
                         </div>
-                        <div className="flex justify-center gap-5">
+                        <div className="flex justify-between md:justify-center gap-5 md:gap-3">
                             <Button text={text} icon={icon} onClick={onClick} />
                             <ButtonSecondary text2={text2} icon2={icon2} onClick2={onClick2} />
                         </div>
